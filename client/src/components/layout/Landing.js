7af0c21bbb3737f05649fcb4dae7ad0d19dfeb53
Mikhail Khusnudinov <mikh.khu@gmail.com>
2019-10-24 18:24:46 +0300

    release-0

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -8,13 +8,11 @@ class Landing extends Component {
         <div className="row">
           <div className="col s12 center-align">
             <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
+              Like {" "}
+              <span style={{ fontFamily: "monospace" }}><b> Evernote</b></span> but <b>Better</b>
             </h4>
             <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
+              Just try it
             </p>
             <br />
             <div className="col s6">
